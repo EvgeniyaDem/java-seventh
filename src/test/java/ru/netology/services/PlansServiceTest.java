07b95f48a7a8fe41018ff1ffacf5c6e17ea20738
Evgeniya D. <Evgeniya7000@rambler.ru>
2024-02-12 15:28:36 +1200

    added parameterized test

--- src/test/java/ru/netology/services/PlansServiceTest.java
+++ src/test/java/ru/netology/services/PlansServiceTest.java
@@ -2,27 +2,26 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class PlansServiceTest {
-    @Test
-    public void firstOption() {
+    @ParameterizedTest
+    //@CsvSource({
+    //    "10000, 3000, 20000, 3",
+    //   "100000, 60000, 150000, 2"
+    //})
+    @CsvFileSource(files = "src/test/resources/plans.csv")
+    public void firstOption(int income, int expenses, int treshold, int expected) {
         PlansService service = new PlansService();
-        int income = 10_000;
-        int expenses = 3_000;
-        int treshold = 20_000;
-        int expected = 3;
+        //int income = 10_000;
+        //int expenses = 3_000;
+        //int treshold = 20_000;
+        //int expected = 3;
         int actual = service.calculate(income, expenses, treshold);
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void secondOption() {
-        PlansService service = new PlansService();
-        int income = 100_000;
-        int expenses = 60_000;
-        int treshold = 150_000;
-        int expected = 2;
-        int actual = service.calculate(income, expenses, treshold);
-        Assertions.assertEquals(expected, actual);
-    }
+
 }
